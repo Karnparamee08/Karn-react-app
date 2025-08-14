43cf5e4ef9a5b13b74af728b8f1fa150b46d5a24
unknown <karnparamee@gmail.com>
2025-08-14 12:01:42 +0700

    lab05

---
+++ app/routes/conponents/data/termProject.tsx
@@ -0,0 +1,26 @@
+ export const myTetmProj = [ // {} - Objects
+      {
+        tpId: 1,
+        tpSubj: "wed Technology",
+        tpDesc: "พัฒนาเว็บไซต์ xxx",
+        tpCover: "/images/Tp01.png",
+        tpUrl: "http://busitlab.rmutto.ac.th/~026730491007-3/",
+        tpTeam: true 
+      },  
+      {
+        tpId: 2,
+        tpSubj: "Computer Programming 1",
+        tpDesc: "",
+        tpCover: "/images/Tp01.png",
+        tpUrl: "http://busitlab.rmutto.ac.th/~026730491007-3/",
+        tpTeam: false
+      },  
+      {
+        tpId: 3,
+        tpSubj: "Computer Programming 2",
+        tpDesc: "",
+        tpCover: "/images/Tp01.png",
+        tpUrl: "http://busitlab.rmutto.ac.th/~026730491007-3/",
+        tpTeam: true
+      },  
+    ]
