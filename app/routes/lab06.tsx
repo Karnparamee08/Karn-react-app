b36b2f789a0850ec0fa14d049f6639d0434ead38
unknown <karnparamee@gmail.com>
2025-08-21 12:12:19 +0700

    lab06

--- app/routes/lab06.tsx
+++ app/routes/lab06.tsx
@@ -89,6 +89,12 @@ export default function myTermProject () {
         });
 
     }
+
+    const resetForm = (e) => {
+        setBook({
+        bookTitle:"", bookDesc:"",bookCover: "", bookPrice: 0.0, bookAuthor: ""
+        });
+    }
     return (
         <>
         <Header title={myPage} />
@@ -143,7 +149,8 @@ export default function myTermProject () {
           <div className="mb-5">
             <button type="button" className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
             onClick={addBook}>Add</button>
-            <button type="button" className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Clear</button>
+            <button type="button" className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+            onClick={resetForm}>Clear</button>
         
           </div>
           
