f490364470ca44fc6c0b494a1ff850f53e5d7b12
unknown <karnparamee@gmail.com>
2025-09-18 12:00:28 +0700

    books.add-book

---
+++ app/server/api.js
@@ -0,0 +1,24 @@
+import express from 'express';
+
+const app = express()
+const port = 3000
+
+let books = [
+    { id: 1, title: "Web Technology", author: "Karn_Paramee 1"},
+    { id: 2, title: "Web Programing", author: "Karn_Paramee 2"},
+];
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.post('/api/books',(req,res) => {
+    const { title, author } = req.body;
+    const newBook = { id: String(books.length + 1),title, author };
+    books.push(newBook);
+    res.status(201).json(newBook);
+})
+
+app.listen(port, () => {
+console.log(`Example app listening on port${port}`)
+})
